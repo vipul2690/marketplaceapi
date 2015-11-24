30fbdd6e0e0ea13f4dd996ac08213fc62980e158
Keshav Lawande <keshav.lawande@presentsoft.co.in>
2015-11-24 13:04:19 +0530

    First Test

--- Gemfile
+++ Gemfile
@@ -48,3 +48,9 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+group :test do
+  gem "rspec-rails", "~> 2.14"
+  gem "factory_girl_rails"
+  gem 'ffaker'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+require 'api_constraints'
+
 Marketplaceapi::Application.routes.draw do
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
 
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+
+    end
   end
 end
