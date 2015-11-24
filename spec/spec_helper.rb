9aa2464df00caf5052accbffd69027e0341f90fb
Keshav Lawande <keshav.lawande@presentsoft.co.in>
2015-11-24 16:58:48 +0530

    Refactors the json parse method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -59,6 +59,7 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/v/3-0/docs
   config.infer_spec_type_from_file_location!
 
+  config.include Request::JsonHelpers, :type => :controller
 
 end
 
