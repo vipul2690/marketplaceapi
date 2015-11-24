78dcd0e7b8c4e2decc1c33222aa932225a6b24d5
Keshav Lawande <keshav.lawande@presentsoft.co.in>
2015-11-24 18:16:17 +0530

    Adds user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,30 @@ describe User do
   it { should be_valid }
 
   it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email) }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
+
+  it { should respond_to(:auth_token) }
+  it { should validate_uniqueness_of(:auth_token) }
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "generate authentication_token!" do
+    it "generates a unique token" do
+      Devise.stub(:friendly_token).and_return("auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eql "auniquetoken123"
+    end
+
+    it "generates another token when one already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to equal existing_user.auth_token
+    end
+  end
 end
 
-describe "when email is not present" do
-  before { @user.email = " " }
-  it { should_not be_valid }
-end
+
