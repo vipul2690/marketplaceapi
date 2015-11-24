a24d83a2ff3426b8025c8df61cb6dbcb7fb44edb
Keshav Lawande <keshav.lawande@presentsoft.co.in>
2015-11-24 14:54:26 +0530

    Adds show action the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,37 +1,19 @@
-describe "POST #create" do
+require 'spec_helper'
 
-  context "when is successfully created" do
+describe Api::V1::UsersController do
+  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
+
+  describe "GET #show" do
     before(:each) do
-      @user_attributes = FactoryGirl.attributes_for :user
-      post :create, { user: @user_attributes }, format: :json
+      @user = FactoryGirl.create :user
+      get :show, id: @user.id, format: :json
     end
 
-    it "renders the json representation for the user record just created" do
+    it "returns the information about a reporter on a hash" do
       user_response = JSON.parse(response.body, symbolize_names: true)
-      expect(user_response[:email]).to eql @user_attributes[:email]
+      expect(user_response[:email]).to eql @user.email
     end
 
-    it { should respond_with 201 }
-  end
-
-  context "when is not created" do
-    before(:each) do
-      #notice I'm not including the email
-      @invalid_user_attributes = { password: "12345678",
-                                   password_confirmation: "12345678" }
-      post :create, { user: @invalid_user_attributes }, format: :json
-    end
-
-    it "renders an errors json" do
-      user_response = JSON.parse(response.body, symbolize_names: true)
-      expect(user_response).to have_key(:errors)
-    end
-
-    it "renders the json errors on why the user could not be created" do
-      user_response = JSON.parse(response.body, symbolize_names: true)
-      expect(user_response[:errors][:email]).to include "can't be blank"
-    end
-
-    it { should respond_with 422 }
+    it { should respond_with 200 }
   end
 end
